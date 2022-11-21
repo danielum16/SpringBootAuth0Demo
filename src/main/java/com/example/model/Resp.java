a8a4c12b5f91eebf59a6f071ae7059d2a4b7979e
danielum16 <danielum.16@gmail.com>
2022-11-20 21:55:41 -0500

    second commit

---
+++ src/main/java/com/example/model/Resp.java
@@ -0,0 +1,14 @@
+package com.example.model;
+
+public class Resp {
+    private final String resp;
+
+    public Resp(String resp) {
+        this.resp = resp;
+    }
+
+    @SuppressWarnings("unused")
+    public String getResp() {
+        return this.resp;
+    }
+}
---
+++ src/main/java/com/example/web/APIController.java
@@ -0,0 +1,37 @@
+package com.example.web;
+
+import org.springframework.web.bind.annotation.RequestParam;
+import com.example.model.Resp;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@RestController
+@RequestMapping(path = "api", produces = MediaType.APPLICATION_JSON_VALUE)
+// For simplicity of this sample, allow all origins. Real applications should
+// configure CORS for their use case.
+@CrossOrigin(origins = "*")
+
+public class APIController {
+
+	public static void main(String[] args) {
+		SpringApplication.run(APIController.class, args);
+	}
+
+	// debug
+	// @GetMapping(value = "/public")
+	// public Resp publicEndpoint() {
+	// return new Resp("All good. You DO NOT need to be authenticated to
+	// call/api/public.");
+	// }
+
+	@GetMapping(value = "/private")
+	public Resp privateEndpoint() {
+		return new Resp("All good. You can see this because you are Authenticated.");
+	}
+}
